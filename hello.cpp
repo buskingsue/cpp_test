e66e8d01ae962e733a9a98859d17f58cbd519b78
buskingsue <abcdzip@naver.com>
2026-01-14 11:11:36 +0900

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!\n";
+    return 0;
+}
+//g++ -Wall -Werror -Wextra -o hello hello.cpp | ./hello
+// vi ~/.zshrc
+// alias gpp='g++ -Wall -Werror -Wextra'
+// source ~/.zshrc
