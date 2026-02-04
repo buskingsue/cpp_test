313140e2efcc1669113652334c6a387679f3eddd
buskingsue <abcdzip@naver.com>
2026-02-04 10:24:20 +0900

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, world!\n";
+    cout << "동수 안녕!\n";
+    cout << "동수 너는 천재!\n";
+    cout << "동수 너는 누구냐!\n";
+
+
     return 0;
 }
-//g++ -Wall -Werror -Wextra -o hello hello.cpp | ./hello
+//g++ -Wall -Werror -Wextra -o hello hello.cpp ; ./hello
 // vi ~/.zshrc
 // alias gpp='g++ -Wall -Werror -Wextra'
 // source ~/.zshrc
